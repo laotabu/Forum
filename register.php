6380c58ddff3e12bbf5bc0402c51e403454144bd
li1030588477 <932978775@qq.com>
2019-12-21 23:16:40 +0800

    更新注册页面，后台通了

--- register.php
+++ register.php
@@ -8,40 +8,39 @@ header("content-type:text/html;charset=utf-8");
 		$u_id=$_POST['u_id'];
 		$u_email=$_POST['u_email'];
 		$u_phone=$_POST['u_phone'];
-		$type=$_POST['type'];
+		$u_type=$_POST['u_type'];
 		
 		/* 检测 用户名，邮箱，手机号 是否存在*/
 		$mysql=new Mysql();
-		$sql="select count(*) as sum from `user` where u_id='$u_id'";
-		$result=$mysql->queryOne($sql);
-		if($result['sum']!=0){
+		$sql="select count(*) as sum from user where u_id='$u_id'";
+		$result=$mysql->exec($sql);
+		if($result){
 			echo '该学号已经注册，请直接登陆！';
 			exit;
 		}else{
-			$sql="select count(*) as sum from `user` where u_name='$u_name'";
-			$result=$mysql->queryOne($sql);
-			if($result['sum']!=0){
+			$sql="select count(*) as sum from user where u_name='$u_name'";
+			$result=$mysql->exec($sql);
+			if($result['sum']){
 				echo '用户名已被注册，请重新输入！';
 				exit;
 			}else{
-				$sql="select count(*) as sum from `user` where u_email='$u_email'";
-				$result=$mysql->queryOne($sql);
-				if($result['sum']!=0){
+				$sql="select count(*) as sum from user where u_email='$u_email'";
+				$result=$mysql->exec($sql);
+				if($result['sum']){
 					echo 'u_email已被注册，请重新输入！';
 					exit;
 				}else{
-					$sql="select count(*) as sum from `user` where u_phone='$u_phone'";
-					$result=$mysql->queryOne($sql);
-					if($result['sum']!=0){
+					$sql="select count(*) as sum from user where u_phone='$u_phone'";
+					$result=$mysql->exec($sql);
+					if($result['sum']){
 						echo '手机号已被注册，请重新输入！';
 						exit;
 					}else{
-						$sql="insert into `user`(`u_id`,`u_name`,`u_passwd`,`u_email`,`u_phone`,`user_type`)value ('$u_id','$u_name','$u_passwd','$u_email','$u_phone','$type')";
-						$result=$mysql->exec($sql);
-						if($result=1){
+						$sql="insert into user(u_id,u_name,u_passwd,u_email,u_phone,u_type) values ('$u_id','$u_name','$u_passwd','$u_email','$u_phone','$u_type')";
+						if($mysql->exec($sql)>0){
 							echo 1;
 						}else{
-							echo '注册失败！';
+							echo mysql_error();
 						}
 			
 					}
