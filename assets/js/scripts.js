4d48f4704dfb2d62a9672f1ceda800554f7e00bc
li1030588477 <932978775@qq.com>
2019-12-21 22:02:49 +0800

    upload the file of css,js,pictures,fonts

---
+++ assets/js/scripts.js
@@ -0,0 +1,31 @@
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    $.backstretch("assets/img/backgrounds/1.jpg");
+    
+    /*
+        Form validation
+    */
+    $('.login-form input[type="text"],.login-form input[type="email"],.login-form input[type="phone"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.login-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], input[type="password"],input[type="email"],input[type="phone"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    
+});
