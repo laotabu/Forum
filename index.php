c5913e146bc282dffdf490cc8b6393c696772f6f
li1030588477 <932978775@qq.com>
2019-12-24 14:51:33 +0800

    更新index页面

--- index.php
+++ index.php
@@ -1,45 +1,50 @@
 <?php
 	header("content-type:text/html;charset=utf-8"); 
 	$pach= $_SERVER['DOCUMENT_ROOT'];
-	include $pach.'/Forum/MYSQL.php';
+	include $pach.'/Forum/utils/MYSQL.php';
 	$mysql=new Mysql();
 	if(!empty($_POST)){
 		$data=$_POST['data'];
 		if($data=='allModule'){
-			$sql="select * from `module`";
+			$sql="select * from module";
 			$result=$mysql->queryAll($sql);
-			echo json_encode($result);
+			echo $result;
 		}else if($data=='allPost'){
 			$sql="select * from `post` where `audit_result`=1 order by id desc limit 6";/*  order by id desc 按照id倒序输出 */
 			$result=$mysql->queryAll($sql);
-			echo json_encode($result);
-		}else if($data=='hotPost'){
-			$sql="select * from `post` where `audit_result`=1 order by post_hot desc limit 6";/* 点赞数越高排名越前 所以要倒序输出 */
+			echo $result;
+		}/*else if($data=='hotPost'){
+			$sql="select * from post where audit_result=1 order by post_hot desc limit 6";
+			/* 点赞数越高排名越前 所以要倒序输出 
 			$result=$mysql->queryAll($sql);
-			echo json_encode($result);
-		}else if($data=='user'){ //普通用户信息
+			echo $result;
+		}*/else if($data=='user'){ //普通用户信息
 			session_start();
-			if(isset($_SESSION['username'])&&$_SESSION['user_type']==2){
-				$username=$_SESSION['username'];
-				$sql="select * from `user` where `username`='$username'";
-				$data=$mysql->queryOne($sql);
+			if(isset($_SESSION['u_name'])&&$_SESSION['u_type']==2){
+				$u_name=$_SESSION['u_name'];
+
+				$sql="select * from user where u_name='$u_name'";
+				$data=$mysql->exec($sql);
+				//echo "<script>alert($data[0])</script>";
 				echo json_encode($data);
 			}else{
 				echo 1;
 			}
 		}else if($data=='public_user'){ //普通用户信息
 			session_start();
-			if(isset($_SESSION['username'])&&$_SESSION['user_type']==1){
-				$username=$_SESSION['username'];
-				$sql="select * from `user` where `username`='$username'";
-				$data=$mysql->queryOne($sql);
+			if(isset($_SESSION['u_name'])&&$_SESSION['user_type']==1){
+				$u_name=$_SESSION['u_name'];
+
+				$sql="select * from `user` where `u_name`='$u_name'";
+				$data=$mysql->exec($sql);
+
 				echo json_encode($data);
 			}else{
 				echo 1;
 			}
 		}else if($data=='notice'){ //公告
 			$sql="select * from `notice`";
-			$data=$mysql->queryOne($sql);
+			$data=$mysql->exec($sql);
 			echo json_encode($data);
 		}else if($data=='banner'){ //banner
 			$sql="select * from `banner`";
@@ -50,8 +55,8 @@
 	if(!empty($_GET)){
 		if(isset($_GET['back'])){
 			session_start();
-			if(isset($_SESSION['username'])){
-				unset($_SESSION['username']);
+			if(isset($_SESSION['u_name'])){
+				unset($_SESSION['u_name']);
 			}
 			if(isset($_SESSION['student_number'])){
 				unset($_SESSION['student_number']);
