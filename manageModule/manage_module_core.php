c8517f11009321bb2a17b363a039fbc0fd5ab9c5
Keen <45956911+WatchKeen@users.noreply.github.com>
2019-12-28 16:30:35 +0800

    Update manage_module_core.php

--- manageModule/manage_module_core.php
+++ manageModule/manage_module_core.php
@@ -2,6 +2,7 @@
 	header("content-type:text/html;charset=utf-8");
 	$pach= $_SERVER['DOCUMENT_ROOT'];
 	include $pach.'/Forum/utils/MYSQL.php';
+
 	$mysql=new Mysql();
 	//删除
 	if(!empty($_GET)){
@@ -13,29 +14,29 @@
 			if($result==1){
 				header('location:manage_module.php');
 			}else{
-				exit('数据库出错！');
+				exit('数据库出错！1');
 			}
 		}else if($op==2){ //帖子禁止
 			$id=$_GET['id'];
 			$reason=$_GET['reason'];
-			$sql="update post set audit_result=10,audit=1,reason='$reason' where Id='$id'";
+			$sql="update post set audit_result=10,audit=1,reason='$reason' where post_module_id ='$id'";
 			$result=$mysql->exec($sql);
 			if($result==1){
 				$referer = $_SERVER['HTTP_REFERER']; //来路信息。就是上一页
-				header("Location: '$referer'"); //浏览器跳转
+				header('location:manage_oldPost.php'); //浏览器跳转
 			}else{
-				exit('数据库出错！');
+				exit('数据库出错！2');
 			}
 		}else if($op==20){ //帖子 审核不通过
 			$id=$_GET['id'];
 			$reason=$_GET['reason'];
-			$sql="update post set audit_result=0,audit=1,reason='$reason' where Id='$id'";
+			$sql="update post set audit_result=0,audit=1,reason='$reason' where post_module_id ='$id'";
 			$result=$mysql->exec($sql);
 			if($result==1){
 				$referer = $_SERVER['HTTP_REFERER']; //来路信息。就是上一页
-				header("Location: '$referer'"); //浏览器跳转
+				header("location:manage_newPost.php"); //浏览器跳转
 			}else{
-				exit('数据库出错！');
+				exit('数据库出错！3');
 			}
 		}else if($op==3){ //删除用户
 			$id=$_GET['u_id'];
@@ -44,7 +45,7 @@
 			if($result==1){
 				header('location:manage_user.php');
 			}else{
-				exit('数据库出错！');
+				exit('数据库出错！4');
 			}
 		}else if($op==4){
 			$id=$_GET['u_id'];
@@ -57,7 +58,7 @@
 				if($result==1){
 					header('location:manage_user.php');
 				}else{
-					exit('数据库出错！');
+					exit('数据库出错！5');
 				}
 			}else{
 				$sql="update user set is_enable=0, is_enable=2 where u_id=$id";
@@ -65,7 +66,7 @@
 				if($result==1){
 					header('location:manage_user.php');
 				}else{
-					exit('数据库出错！');
+					exit('数据库出错！6');
 				}
 			}
 		}else if($op==5){
@@ -73,18 +74,18 @@
 			$sql="update `notice` set `notice`='$notice'";
 			$result=$mysql->exec($sql);
 			if($result==1){
-				header('location:notice.php');
+				header('location:manage_notice.php');
 			}else{
 				exit('公告最多发布30字数！请重新输入！');
 			}
 		}else if($op==6){
 			$id=$_GET['id'];
-			$sql="update `post` set `audit_result`=1,`audit`=1 where `id`='$id'";
+			$sql="update `post` set `audit_result`=1,`audit`=1 where post_module_id ='$id'";
 			$result=$mysql->exec($sql);
 			if($result==1){
-				header('location:newPost.php');
+				header('location:manage_newPost.php');
 			}else{
-				exit('数据库出错！');
+				exit('数据库出错！7');
 			}
 		}else if($op==7){//管理员下架商品
 			$id=$_GET['id'];
@@ -127,7 +128,7 @@
 			if($result==1){
 				header('location:manage_module.php');
 			}else{
-				exit('数据库出错！');
+				exit('数据库出错！8');
 			}
 		}else if($op==2){//数据插入
 			$module_type=$_POST['module_type'];
@@ -143,7 +144,7 @@
 					echo "<script> alert('添加成功'); </script>";
 					echo "<meta http-equiv='Refresh' content='0;URL=manage_module.php'>";
 				}else{
-					exit('数据库出错！');
+					exit('数据库出错！9');
 				}
 			}else{
 				echo "<script> alert('所添加已经存在'); </script>";
@@ -162,7 +163,7 @@
 			echo json_encode($data);
 		}else if($op==5){ // 帖子取消禁止
 			$id=$_POST['id'];
-			$sql="update `post` set `audit_result`=1 , `reason`=0 where id=$id";
+			$sql="update `post` set `audit_result`=1 , `reason`=0 where post_module_id=$id";
 			$result=$mysql->exec($sql);
 			if($result==1){
 				echo 1;
@@ -180,7 +181,7 @@
 			}
 		}else if($op==7){ // 帖子撤销举报
 			$id=$_POST['id'];
-			$sql="update `post` set `inform`=0 where id=$id";
+			$sql="update `post` set `inform`=0 where  post_module_id =$id";
 			$result=$mysql->exec($sql);
 			if($result==1){
 				echo 1;
